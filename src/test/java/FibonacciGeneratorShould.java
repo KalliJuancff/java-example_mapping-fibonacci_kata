075933251cf81e301626fcd9b9a26fca297a54ba
Juan Carlos Fidalgo Fernández <juancfoofn@yahoo.com>
2024-10-27 10:52:24 +0100

    refactor(tests): convert into parameterized test

--- src/test/java/FibonacciGeneratorShould.java
+++ src/test/java/FibonacciGeneratorShould.java
@@ -14,19 +14,14 @@ public class FibonacciGeneratorShould {
         assertThat(getFibonacciNumberFor(index)).isEqualTo(expected);
     }
 
-    @Test
-    public void return_1_when_asked_for_the_number_with_index_2() {
-        assertThat(getFibonacciNumberFor(2)).isEqualTo(1);
-    }
-
-    @Test
-    public void return_2_when_asked_for_the_number_with_index_3() {
-        assertThat(getFibonacciNumberFor(3)).isEqualTo(2);
-    }
-
-    @Test
-    public void return_5_when_asked_for_the_number_with_index_5() {
-        assertThat(getFibonacciNumberFor(5)).isEqualTo(5);
+    @ParameterizedTest
+    @CsvSource({
+            "2, 1",
+            "3, 2",
+            "5, 5"
+    })
+    public void generate_sum_of_two_previous_values_from_third_onwards(int index, int expected) {
+        assertThat(getFibonacciNumberFor(index)).isEqualTo(expected);
     }
 
     private static int getFibonacciNumberFor(int index) {
